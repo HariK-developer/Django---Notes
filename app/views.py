00d2e4927d38bb7388ead67154c5f35f0cc8f78b
HariK-developer <hariswdeveloper@gmail.com>
2024-03-19 16:20:33 +0530

    search api

--- app/views.py
+++ app/views.py
@@ -1,21 +1,17 @@
 from rest_framework import generics
 from .models import Note
 from .serializers import NoteSerializer
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import filters
 
 
 class NoteList(generics.ListCreateAPIView):
+    search_fields = ['title','category']
+    filter_backends = (filters.SearchFilter,)
     queryset = Note.objects.valid_data()
     serializer_class = NoteSerializer
     
-    def list(self,*args, **kwargs):
-        queryset = self.get_queryset()
-        serializer = self.get_serializer(queryset, many=True)
-        response = {"success": True,"data": serializer.data,"status":status.HTTP_200_OK}
-        return Response(response)
     
-        
+    
 class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Note.objects.valid_data()
     serializer_class = NoteSerializer
